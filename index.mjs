d6e169c82d59751aba7e03b60568e4564d64a462
jente_tav <54480755+jente01t@users.noreply.github.com>
2024-12-16 14:10:49 +0100

    Initial setup express, MongoDB(atlas)

---
+++ index.mjs
@@ -0,0 +1,20 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import mongoose from 'mongoose';
+
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+
+
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true })
+  .then(() => console.log('Connected to MongoDB'))
+  .catch(err => console.error('Failed to connect to MongoDB:', err));
+
+app.get('/', (req, res) => {
+  res.send('Hello World');
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
