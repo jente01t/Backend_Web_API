0d545caaf768946117f744056babb0418fc52c39
jente_tav <54480755+jente01t@users.noreply.github.com>
2024-12-21 09:03:46 +0100

    CRUD operations for user

--- controllers/userController.mjs
+++ controllers/userController.mjs
@@ -25,4 +25,59 @@ export const loginUser = async (req, res) => {
     } catch (error) {
         res.status(400).json({message: error.message});
     }
+};
+
+export const getAllUsers = async (req, res) => {
+    try {
+        const usersname = await User.find({}).select('username');
+        res.status(200).json(usersname);
+    } catch (error) {
+        res.status(400).json({message: error.message});
+    }
+};
+
+export const getUserById = async (req, res) => {
+    try {
+        const userID = await User.findById(req.params.id);
+        if (userID) {
+            res.status(200).json(userID);
+        } else {
+            res.status(404).json({message: 'User not found'});
+        }
+    } catch (error) {
+        res.status(400).json({message: error.message});
+    }
+};
+
+export const updateUser = async (req, res) => {
+    try {
+        const user = await User.findById(req.params.id);
+        if (user) {
+            user.username = req.body.username || user.username;
+            if (req.body.password) {
+                user.password = req.body.password;
+            }
+            await user.save();
+            res.status(200).json({message: 'User updated successfully'});
+        } else {
+            res.status(404).json({message: 'User not found'});
+        }
+    } catch (error) {
+        res.status(400).json({message: error.message});
+    }
+};
+
+export const deleteUser = async (req, res) => {
+    try {
+        const user = await User.findById(req.params.id);
+        if (user) {
+            await user.deleteOne();
+            res.status(200).json({message: 'User deleted successfully'});
+        } else {
+            res.status(404).json({message: 'User not found'});
+        }
+    } catch (error) {
+        res.status(400).json({message: error.message});
+    }
 }
+
--- routes/userRoutes.mjs
+++ routes/userRoutes.mjs
@@ -1,9 +1,19 @@
 import express from 'express';
-import {registerUser, loginUser} from '../controllers/userController.mjs';
+import {registerUser, 
+        loginUser,
+        getAllUsers,
+        getUserById,
+        updateUser,
+        deleteUser
+} from '../controllers/userController.mjs';
 
 const router = express.Router();
 
 router.post('/register', registerUser);
 router.post('/login', loginUser);
+router.get('/', getAllUsers);
+router.get('/:id', getUserById);
+router.put('/:id', updateUser);
+router.delete('/:id', deleteUser);
 
 export default router;
